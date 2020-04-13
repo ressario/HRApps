11f2b431a22c9adc7f19c1c051aa79cf93a4c624
ressario <ressario@gmail.com>
2020-04-13 13:46:28 +0700

    HR - Initialization

---
+++ HR_API/Data/HRContext.cs
@@ -0,0 +1,20 @@
+﻿using HR_API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HR_API.Data
+{
+    public class HRContext : DbContext
+    {
+        public HRContext(DbContextOptions options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Leave> Leaves { get; set; }
+    }
+}
---
+++ HR_Web/Controllers/LeaveController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HR_Web.Controllers
+{
+    public class LeaveController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Submission()
+        {
+            return View();
+        }
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
